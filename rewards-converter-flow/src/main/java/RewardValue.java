6f1ba020af73f787d2eeecb9877757fe6c55ee43
YASMEEN <yasmeennaaz@gmail.com>
2023-12-21 22:39:00 +0530

    Added RewardValue class and corresponding tests

--- rewards-converter-flow/src/main/java/RewardValue.java
+++ rewards-converter-flow/src/main/java/RewardValue.java
@@ -1,4 +1,24 @@
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 public class RewardValue {
+    @Test
+    public void testCashToMilesConversion() {
+        double cashAmount = 100.0;
+        RewardValue rewardValue = new RewardValue(cashAmount);
+        double expectedMiles = cashAmount / 0.0035; // Using the conversion rate from your class
+        double actualMiles = rewardValue.getMilesValue();
+        assertEquals(expectedMiles, actualMiles, 0.001); // Adjust delta as needed
+    }
+
+    @Test
+    public void testMilesToCashConversion() {
+        int miles = 500;
+        RewardValue rewardValue = new RewardValue(miles);
+        double expectedCash = miles * 0.0035; // Using the conversion rate from your class
+        double actualCash = rewardValue.getCashValue();
+        assertEquals(expectedCash, actualCash, 0.001); // Adjust delta as needed
+    }
     private double cashValue;
 
     // Constructor that accepts cash value
